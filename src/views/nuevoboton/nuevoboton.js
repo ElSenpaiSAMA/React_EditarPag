04e14ad18f5af962023d9e9e7d73288660082df1
elsenpaisama <mnicolas03sp@gmail.com>
2024-10-27 21:44:27 +0100

    first commit

---
+++ src/views/nuevoboton/nuevoboton.js
@@ -0,0 +1,220 @@
+import React from 'react'
+import classNames from 'classnames'
+
+import {
+  CAvatar,
+  CButton,
+  CButtonGroup,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CCol,
+  CProgress,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import {
+  cibCcAmex,
+  cibCcApplePay,
+  cibCcMastercard,
+  cibCcPaypal,
+  cibCcStripe,
+  cibCcVisa,
+  cibGoogle,
+  cibFacebook,
+  cibLinkedin,
+  cifBr,
+  cifEs,
+  cifFr,
+  cifIn,
+  cifPl,
+  cifUs,
+  cibTwitter,
+  cilCloudDownload,
+  cilPeople,
+  cilUser,
+  cilUserFemale,
+} from '@coreui/icons'
+
+import avatar1 from 'src/assets/images/avatars/1.jpg'
+import avatar2 from 'src/assets/images/avatars/2.jpg'
+import avatar3 from 'src/assets/images/avatars/3.jpg'
+import avatar4 from 'src/assets/images/avatars/4.jpg'
+import avatar5 from 'src/assets/images/avatars/5.jpg'
+import avatar6 from 'src/assets/images/avatars/6.jpg'
+
+import WidgetsBrand from '../widgets/WidgetsBrand'
+import WidgetsDropdown from '../widgets/WidgetsDropdown'
+import MainChart from './MainChart'
+
+const nuevoboton = () => {
+  const progressExample = [
+    { title: 'Visits', value: '29.703 Users', percent: 40, color: 'success' },
+    { title: 'Unique', value: '24.093 Users', percent: 20, color: 'info' },
+    { title: 'Pageviews', value: '78.706 Views', percent: 60, color: 'warning' },
+    { title: 'New Users', value: '22.123 Users', percent: 80, color: 'danger' },
+    { title: 'Bounce Rate', value: 'Average Rate', percent: 40.15, color: 'primary' },
+  ]
+
+  const progressGroupExample1 = [
+    { title: 'Monday', value1: 34, value2: 78 },
+    { title: 'Tuesday', value1: 56, value2: 94 },
+    { title: 'Wednesday', value1: 12, value2: 67 },
+    { title: 'Thursday', value1: 43, value2: 91 },
+    { title: 'Friday', value1: 22, value2: 73 },
+    { title: 'Saturday', value1: 53, value2: 82 },
+    { title: 'Sunday', value1: 9, value2: 69 },
+  ]
+
+  const progressGroupExample2 = [
+    { title: 'Male', icon: cilUser, value: 53 },
+    { title: 'Female', icon: cilUserFemale, value: 43 },
+  ]
+
+  const progressGroupExample3 = [
+    { title: 'Organic Search', icon: cibGoogle, percent: 56, value: '191,235' },
+    { title: 'Facebook', icon: cibFacebook, percent: 15, value: '51,223' },
+    { title: 'Twitter', icon: cibTwitter, percent: 11, value: '37,564' },
+    { title: 'LinkedIn', icon: cibLinkedin, percent: 8, value: '27,319' },
+  ]
+
+  const tableExample = [
+    {
+      avatar: { src: avatar1, status: 'success' },
+      user: {
+        name: 'Yiorgos Avraamu',
+        new: true,
+        registered: 'Jan 1, 2023',
+      },
+      country: { name: 'USA', flag: cifUs },
+      usage: {
+        value: 50,
+        period: 'Jun 11, 2023 - Jul 10, 2023',
+        color: 'success',
+      },
+      payment: { name: 'Mastercard', icon: cibCcMastercard },
+      activity: '10 sec ago',
+    },
+    {
+      avatar: { src: avatar2, status: 'danger' },
+      user: {
+        name: 'Avram Tarasios',
+        new: false,
+        registered: 'Jan 1, 2023',
+      },
+      country: { name: 'Brazil', flag: cifBr },
+      usage: {
+        value: 22,
+        period: 'Jun 11, 2023 - Jul 10, 2023',
+        color: 'info',
+      },
+      payment: { name: 'Visa', icon: cibCcVisa },
+      activity: '5 minutes ago',
+    },
+    {
+      avatar: { src: avatar3, status: 'warning' },
+      user: { name: 'Quintin Ed', new: true, registered: 'Jan 1, 2023' },
+      country: { name: 'India', flag: cifIn },
+      usage: {
+        value: 74,
+        period: 'Jun 11, 2023 - Jul 10, 2023',
+        color: 'warning',
+      },
+      payment: { name: 'Stripe', icon: cibCcStripe },
+      activity: '1 hour ago',
+    },
+    {
+      avatar: { src: avatar4, status: 'secondary' },
+      user: { name: 'Enéas Kwadwo', new: true, registered: 'Jan 1, 2023' },
+      country: { name: 'France', flag: cifFr },
+      usage: {
+        value: 98,
+        period: 'Jun 11, 2023 - Jul 10, 2023',
+        color: 'danger',
+      },
+      payment: { name: 'PayPal', icon: cibCcPaypal },
+      activity: 'Last month',
+    },
+    {
+      avatar: { src: avatar5, status: 'success' },
+      user: {
+        name: 'Agapetus Tadeáš',
+        new: true,
+        registered: 'Jan 1, 2023',
+      },
+      country: { name: 'Spain', flag: cifEs },
+      usage: {
+        value: 22,
+        period: 'Jun 11, 2023 - Jul 10, 2023',
+        color: 'primary',
+      },
+      payment: { name: 'Google Wallet', icon: cibCcApplePay },
+      activity: 'Last week',
+    },
+    {
+      avatar: { src: avatar6, status: 'danger' },
+      user: {
+        name: 'Friderik Dávid',
+        new: true,
+        registered: 'Jan 1, 2023',
+      },
+      country: { name: 'Poland', flag: cifPl },
+      usage: {
+        value: 43,
+        period: 'Jun 11, 2023 - Jul 10, 2023',
+        color: 'success',
+      },
+      payment: { name: 'Amex', icon: cibCcAmex },
+      activity: 'Last week',
+    },
+  ]
+
+  return (
+    <>
+      <h1>Para que disfrutes un rato te dejo una cancion :) </h1>
+      <div>
+        <iframe
+          width="1300"
+          height="700"
+          src="https://www.youtube.com/embed/9bZkp7q19f0?autoplay=1&" 
+          title="Video de YouTube"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div>
+        <h2>2 Razones para jugar League of Legends</h2>
+        <p>
+          1- la razón principal, la razón NÚMERO UNO para jugar a League of Legends está clara: la comunidad, la gente. Gente que se dedica a jugar sin pensar en su equipo y lo que es más «gracioso»,
+          a echarle la culpa a los demás de sus fallos.
+          <br></br>
+          <br></br>
+          Afortunadamente tan solo es un videojuego y cuando pierdes una partida por culpa de alguien que simplemente insulta y no juega en equipo,
+          solo queda resignarse. Fastidia aún más cuando juegas partidas clasificatorias (ranked), en las que una partida decide si subes de división o no.
+          Si pierdes porque el otro equipo ha sido mejor, perfecto. Pero lo cierto es que la mayoría de las partidas se pierden por ese tipo de gente. Una pena.
+          <br></br>
+          <br></br>
+          2- Otra de las razones es el tiempo que «pierdes» o mejor dicho GANAS jugando. 1000 horas son muchas horas, que podían haberse empleado en otras actividades pero sabemos que eso no nos importa realmente.
+          Pero bueno, eso ya depende de cada uno. Si tienes tiempo libre, ¿qué tiene de malo jugar una partidita? ¿O tal vez todo un fin de semana?
+          <br></br>
+          <br></br>
+          En definitiva, League of Legends es un gran MOBA (por no decir el mejor, aunque algunos no estaréis de acuerdo),
+          pero su comunidad es un problema muy grande. A mí me encanta por la importancia de la estrategia en cada partida y la posibilidad de mejorar partida a partida.
+          Y no hace falta hablar de los personajes. Algunos son geniales. Si nunca has jugado y quieres probarlo, ten en cuenta lo que he escrito aquí.
+          Y recuerda: siempre es mejor jugar con amigos, es como realmente se disfruta del juego.
+        </p>
+        <a href='https://www.leagueoflegends.com/es-es/download/'>Descargate el juego se que te encantara</a>
+      </div>
+    </>
+  )
+}
+
+export default nuevoboton
---
+++ src/views/pages/NewPage/newpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CIcon from '@coreui/icons-react'
+import { cilMagnifyingGlass } from '@coreui/icons'
+
+const NewPage = () => {
+  return (
+    <h1>HOLA</h1>
+  );
+};
+
+export default NewPage
